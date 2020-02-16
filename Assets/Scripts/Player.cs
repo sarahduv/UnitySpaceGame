8c12d14bc57f999cf72049c9dda163021f338357
sarahduv <46334082+sarahduv@users.noreply.github.com>
2020-02-15 18:18:02 -0800

    Score UI component complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -27,16 +27,25 @@ public class Player : MonoBehaviour
     private bool _isShieldActive = false;
     [SerializeField]
     private GameObject _shieldVisualizer;
+    [SerializeField]
+    private int _score;
+    private UIManager _uiManager;
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
         _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
 
         if (_spawnManager == null)
         {
             Debug.LogError("The spawn manager is null.");
+        }
+
+        if(_uiManager == null)
+        {
+            Debug.LogError("The UI manager is null.");
         }
     }
 
@@ -151,5 +160,11 @@ public class Player : MonoBehaviour
     {
         _isShieldActive = true;
         _shieldVisualizer.SetActive(true);
+    }
+
+    public void AddToScore(int points)
+    {
+        _score += points;
+        _uiManager.UpdateScore(_score);
     }
 }
